bbbfe1dfdbc3190c0d1c5fbfbfadfc37e2d1e121
康雪烛 <62123890+Heniz19210601@users.noreply.github.com>
2020-03-22 09:29:59 +0800

    Update 疾病传播.py

--- 疾病传播.py
+++ 疾病传播.py
@@ -1,25 +1,25 @@
-//这是一个疾病传播模型。一开始只有一个感染者和一百个健康人，每个人每天只在规定世界内不动或移动一格。
-//当感染者和健康人处在同一坐标时会感染健康人。感染者的治愈几率是1以内随机的小数，如果治愈几率小于治愈值则感染者恢复为健康人。
-//画一张度过最大运行时间之后所有人的分布图吧！
+##这是一个疾病传播模型。一开始只有一个感染者和一百个健康人，每个人每天只在规定世界内不动或移动一格。
+##当感染者和健康人处在同一坐标时会感染健康人。感染者的治愈几率是1以内随机的小数，如果治愈几率小于治愈值则感染者恢复为健康人。
+##画一张度过最大运行时间之后所有人的分布图吧！
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-maxtime = 1000//最大运行时间
-npeople = 100//健康人的数量
-remission = 0//治愈几率
+maxtime = 1000##最大运行时间
+npeople = 100##健康人的数量
+remission = 0##治愈几率
 sidelength = 40
 x = np.arange(0,sidelength+1,1)
 y = np.arange(0,sidelength+1,1)
-world = np.zeros([sidelength,sidelength])//圈定一个移动范围
+world = np.zeros([sidelength,sidelength])##圈定一个移动范围
 
 human = np.zeros(npeople)
-coordinate_human = []//健康人的坐标
+coordinate_human = []##健康人的坐标
 for h in human:
-    coordinate_human.append([np.random.choice(x),np.random.choice(y)])//第一天所有人的随机坐标
+    coordinate_human.append([np.random.choice(x),np.random.choice(y)])##第一天所有人的随机坐标
 
-zombie_initial = [np.random.choice(x),np.random.choice(y)]//第一个感染者的坐标
-coordinate_zombie = []//感染者的坐标
+zombie_initial = [np.random.choice(x),np.random.choice(y)]##第一个感染者的坐标
+coordinate_zombie = []##感染者的坐标
 coordinate_zombie.append(zombie_initial)
 
 x = []
@@ -36,59 +36,82 @@ for n in coordinate_zombie:
 plt.plot(x1, y1, '.', color='lightgreen')
 plt.plot(x, y, '.', color='blue')
 plt.title('The first day for initial locations')
-plt.show() //画出第一天所有健康人和感染者的分布图
+plt.show() ##画出第一天所有健康人和感染者的分布图
 
-def move(x)://单次移动
+def move(x):##单次移动
     list40 = [-1,-1,0]
     list0 = [0,1,1]
-    rand_move = [-1,0,1] //范围内移动
-    for i in range(1):
-        if x[i] >= 40:
-            x[i] += int(np.random.choice(list40))//如果移动后坐标大于40则必须后退或原地不动
-        elif x[i] <= 0 :
-            x[i] += int(np.random.choice(list0))//如果移动后坐标小于0则必须前进或者原地不动
-        else:    
-            x[i] += int(np.random.choice(rand_move))//如果移动后不超出范围则不变
+    list_all = [-1,0,1] ##范围内移动
+    move40 = np.random.choice(list40)
+    move0 = np.random.choice(list0)
+    move_normal = np.random.choice(list_all)
+    if x[0] <= 0:
+        x[0] += int(move0)##移动后x坐标小于0则必须前进或者原地不动
+        if int(move0) == 0:##如果x坐标不移动则考虑y坐标移动
+            if x[1] <= 40:
+                x[1] += int(np.random.choice(list0))
+            elif x[1] >= 40:
+                x[1] += int(np.random.choice(list40))
+            else:
+                x[1] += int(np.random.choice(list_all))
+    elif x[0] >= 40:
+        x[0] += int(move40)##移动后x坐标大于40则必须后退或原地不动
+        if int(move40) == 0:##如果x坐标不移动则考虑y坐标移动
+            if x[1] <= 40:
+                x[1] += int(np.random.choice(list0))
+            elif x[1] >= 40:
+                x[1] += int(np.random.choice(list40))
+            else:
+                x[1] += int(np.random.choice(list_all))
+    else:    
+        x[0] += int(move_normal)##移动后不超出范围
+        if int(move_normal) == 0:##如果x坐标不移动则考虑y坐标移动
+            if x[1] <= 40:
+                x[1] += int(np.random.choice(list0))
+            elif x[1] >= 40:
+                x[1] += int(np.random.choice(list40))
+            else:
+                x[1] += int(np.random.choice(list_all))
     return x
 
-def step(zombie,human)://运行
-    new = []//新生感染者
+def step(zombie,human):##运行
+    new = []##新生感染者
     for i in range(len(zombie)):
         for j in range(len(human)):
             each_zombie = [zombie[i][0],zombie[i][1]]
             each_human = [human[j][0],human[j][1]]
 
             if each_zombie == each_human:
-                new.append(j)//当感染者和健康人在同一坐标时健康人被感染
+                new.append(j)##当感染者和健康人在同一坐标时健康人被感染
 
     for n in new:
         new_zombie = human[n]
-        human.remove(new_zombie)//被感染者从健康人列表中移除
-        zombie.append(new_zombie)//被感染者加入感染者列表
+        human.remove(new_zombie)##被感染者从健康人列表中移除
+        zombie.append(new_zombie)##被感染者加入感染者列表
         
-    reborn = [] //治愈
+    reborn = [] ##治愈
     for i in range(len(zombie)):
-        a = np.random.random(1)//随机的治愈值
+        a = np.random.random(1)##随机的治愈值
         if a < remission:
-            reborn.append(i)//当治愈几率小于治愈值时，感染者恢复为健康人
+            reborn.append(i)##当治愈几率小于治愈值时，感染者恢复为健康人
     
     for j in reborn:
         new_human = reborn[j]
-        zombie.remove(new_human)//恢复者从感染者列表中移除
-        human.append(new_human)//恢复者加入健康人列表
+        zombie.remove(new_human)##恢复者从感染者列表中移除
+        human.append(new_human)##恢复者加入健康人列表
 
     for x in zombie:
-        move(x)//感染者的移动
+        move(x)##感染者的移动
 
     for y in human:
-        move(y)//健康人的移动
+        move(y)##健康人的移动
 
     return zombie,human
 
 maxtime = 1000
 
 for day in range(maxtime):
-    step(coordinate_zombie,coordinate_human)//运行最大天数次数的移动
+    step(coordinate_zombie,coordinate_human)##运行最大天数次数的移动
 
 a = []
 b = []
